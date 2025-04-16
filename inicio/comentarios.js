45fbb856745e21f1b6a51f18e0c6c7d8782affaa
vinnymsk <vi9xmcpe@gmail.com>
2025-04-15 22:25:51 -0300

    Inicio com sentenças blocos e dados

---
+++ inicio/comentarios.js
@@ -0,0 +1,9 @@
+// O console é utilizado para mostrar algo na tela do computador
+// console.log("Passo #03");
+console.log("Passo #01");
+console.log("Passo #02");
+
+/*O console é
+utilizado para 
+mostrar algo na tela 
+do computador*/
---
+++ inicio/desafio_bloco_sentenca.js
@@ -0,0 +1,15 @@
+/* Desafio: Criar dois blocos de código
+O primeiro deve imprimir na tela os números pares de 0 a 10
+E o segundo bloco deve imprimir na tela os ímpares*/
+
+{
+  console.log("Pares: ");
+  console.log("0 | 2 | 4 | 6 | 8 | 10");
+
+}
+
+{
+  console.log("Ímpares: ");
+  console.log("1 | 3 | 5 | 7 | 9");
+
+}
