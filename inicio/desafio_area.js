7d0eb8b998e33c609b671d2e28e0869fb7d1a306
vinnymsk <vi9xmcpe@gmail.com>
2025-04-22 22:25:13 -0300

    Desafio Area e Troca

---
+++ inicio/desafio_area.js
@@ -0,0 +1,6 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio + "m²";
+
+console.log("A área da circunferência é de: " + areaCirc);
+
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,8 @@
+var x = 94;
+var y = 7;
+
+console.log("Primeira variável: " + y);
+console.log("Segunda variável: " + x);
+[x, y] = [y, x];
+console.log("Primeira variável invertida: " + y);
+console.log("Segunda variável invertida: " + x);
