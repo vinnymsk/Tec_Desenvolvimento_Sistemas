3ba7ca4cb33bcc8cf21e9927e36b34f42e6b7c8e
vinnymsk <vi9xmcpe@gmail.com>
2025-04-22 21:00:34 -0300

    Lidando com dados (var, let, const)

---
+++ inicio/dados_2.js
@@ -0,0 +1,13 @@
+console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
++ ", Categoria: " + categoria 
++ ", Preço: " + preco
++ ", Desconto: " + desconto);
---
+++ inicio/dados_3.js
@@ -0,0 +1,15 @@
+let  idade = 15;
+console.log(typeof 15);
+console.log(typeof idade);
+
+let salario = 1512.50;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+let nome = "Vinny";
+console.log(typeof nome)
+
+console.log (1 + 1);
+console.log ("1" + 1);
